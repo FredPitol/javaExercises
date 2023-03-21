a40cdc2196296f6f6b5bbe7537b8985f3e899558
Frederico Pitol <freirefrederico1@gmail.com>
2023-03-21 10:05:31 -0300

    Testing summation

--- testingSummation.java
+++ testingSummation.java
@@ -1,3 +1,18 @@
+import javax.print.event.PrintEvent;
+
 public class testingSummation {
 
+    public static void main(String[] args) {
+         
+        int counter = 0;
+        int total = 0;
+
+        while (counter <= 10 ){
+            total += counter;
+            counter++;
+            System.out.println(counter);
+        }
+        System.out.println(total);
+       
+    }
 }
