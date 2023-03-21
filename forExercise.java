ac48f4a421f9acf4beacea9ae92621971e85c5f8
Frederico Pitol <freirefrederico1@gmail.com>
2023-03-21 10:25:53 -0300

    Counting 1 to 10 with for

---
+++ forExercise.java
@@ -0,0 +1,9 @@
+public class forExercise {
+    //Counting 1 to 10 with for
+    public static void main(String[] args) {
+        for (int counter = 0; counter <= 10; counter++){
+            System.out.println(counter);
+        }
+    }
+    
+}
