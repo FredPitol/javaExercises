c94a60007b4a59cf32b8ffcd41189de5efa06ccb
Frederico Pitol <freirefrederico1@gmail.com>
2023-03-21 10:29:43 -0300

    Code to be fixed

---
+++ fixingErrorWithFor.java
@@ -0,0 +1,12 @@
+public class fixingErrorWithFor {
+    /*"Transform the following code, 
+    which uses a while loop, into code that uses a for loop." */
+    public static void main(String[] args) {
+        int contador = 0;
+        while(contador <= 10) {
+            System.out.println(contador);
+            contador++;
+        }
+    }
+    
+}
